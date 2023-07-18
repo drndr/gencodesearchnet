81ddba99f88d5e536ae03511e6afdfbda6056833
drndr <drndr21@gmail.com>
2023-07-18 17:06:08 +0200

    add adv test set

--- sample_data/clf/codesearchnet_adv/sample.py
+++ sample_data/clf/codesearchnet_adv/sample.py
@@ -3,7 +3,7 @@ import argparse
 import json
 
 
-def select_random_lines_train(input_file, output_file, num_lines=10):
+def select_random_lines(input_file, output_file, num_lines=10):
     with open(input_file, 'r', encoding='utf-8') as infile:
         lines = infile.readlines()
 
@@ -12,7 +12,7 @@ def select_random_lines_train(input_file, output_file, num_lines=10):
     with open(output_file, 'w', encoding='utf-8') as outfile:
         outfile.writelines(random_lines)
 
-def process_jsonl_file_train(input_file, output_file):
+def process_jsonl_file(input_file, output_file):
     processed_data = []
     with open(input_file, 'r', encoding='utf-8') as infile:
         for line in infile:
@@ -21,12 +21,20 @@ def process_jsonl_file_train(input_file, output_file):
             except json.JSONDecodeError as e:
                 print(f"Error: JSON decoding failed - {e}")
                 continue
-
-            processed_item = {
-                "code": ' '.join(item.get("code_tokens", "")),
-                "docstring": ' '.join(item.get("docstring_tokens", "")),
-                "idx": item.get("idx", None)
-            }
+            
+            if "train" in input_file:
+                processed_item = {
+                    "code": ' '.join(item.get("code_tokens", "")),
+                    "docstring": ' '.join(item.get("docstring_tokens", "")),
+                    "idx": item.get("idx", None)
+                }
+                
+            else:
+                processed_item = {
+                    "code": ' '.join(item.get("function_tokens", "")),
+                    "docstring": ' '.join(item.get("docstring_tokens", "")),
+                    "idx": item.get("idx", None)
+                }
             processed_data.append(processed_item)
 
     with open(output_file, 'w', encoding='utf-8') as outfile:
@@ -34,8 +42,11 @@ def process_jsonl_file_train(input_file, output_file):
             json.dump(item, outfile, ensure_ascii=False)
             outfile.write('\n')
 
-def create_cbt_jsonl_file_train(input_file, output_file):
+def create_cbt_jsonl_file(input_file, output_file):
     data_with_fields = []
+    data = []
+
+    # Load data from the input JSONL file
     with open(input_file, 'r', encoding='utf-8') as infile:
         for line in infile:
             try:
@@ -43,16 +54,38 @@ def create_cbt_jsonl_file_train(input_file, output_file):
             except json.JSONDecodeError as e:
                 print(f"Error: JSON decoding failed - {e}")
                 continue
+            data.append(item)
 
-            input_text = item["docstring"] + " [SEP] " + item["code"]
-            target = 1
-            target_options = [0, 1]
+    # Create a list of indices to sample from
+    sample_indices = list(range(len(data)))
 
-            data_with_fields.append({
-                "input": input_text,
-                "target": target,
-                "target_options": target_options
-            })
+    for idx, item in enumerate(data):
+        
+        # Create True Pair
+        input = item["docstring"] + " [SEP] " + item["code"]
+        target = 1
+        target_options = [0, 1]
+        
+        data_with_fields.append({
+            "input": input,
+            "target": target,
+            "target_options": target_options
+        })
+        
+        # Create False Pair
+        sample_indices.remove(idx)  # To avoid sampling from the same line
+        sampled_idx = random.choice(sample_indices) # Randomly sample code from another line
+        input = item["docstring"] + " [SEP] " + data[sampled_idx]["code"]
+        sample_indices.append(idx)  # Add back the removed index for future sampling
+
+        target = 0
+        target_options = [0, 1]
+
+        data_with_fields.append({
+            "input": input,
+            "target": target,
+            "target_options": target_options
+        })
 
     with open(output_file, 'w', encoding='utf-8') as outfile:
         for item in data_with_fields:
@@ -76,18 +109,23 @@ def main():
         cbt_format_file = "train_sample_cbt.jsonl"
         
     
-        select_random_lines_train(input_file, raw_output_file, num_lines_to_select)
-        process_jsonl_file_train(raw_output_file, processed_output_file)
-        create_cbt_jsonl_file_train(processed_output_file, cbt_format_file)
+        select_random_lines(input_file, raw_output_file, num_lines_to_select)
+        process_jsonl_file(raw_output_file, processed_output_file)
+        create_cbt_jsonl_file(processed_output_file, cbt_format_file)
         
         
     elif args.split =="test":
         input_file = 'test.jsonl'
         raw_output_file = 'test_sample_raw.jsonl'
-        processed_output_file = 'train_sample_processed.jsonl'
+        processed_output_file = 'test_sample_processed.jsonl'
+        cbt_format_file = "test_sample_cbt.jsonl"
+        
+        select_random_lines(input_file, raw_output_file, num_lines_to_select)
+        process_jsonl_file(raw_output_file, processed_output_file)
+        create_cbt_jsonl_file(processed_output_file, cbt_format_file)
         
     else:
-        pass
+        print("Invalid split name, use either train or test as argument")
 
 if __name__ == '__main__':
     main()
