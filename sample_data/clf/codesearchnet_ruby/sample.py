7ad7f79db85df8e6617f71bfe8d02f0669dbe628
drndr <drndr21@gmail.com>
2023-07-24 13:20:34 +0200

    add codesearchnet samples

---
+++ sample_data/clf/codesearchnet_ruby/sample.py
@@ -0,0 +1,103 @@
+from datasets import load_dataset
+import random
+import json
+import ast
+
+
+def sample_create_cbt_jsonl_file(dataset, output_file, num_samples=10):
+    sampled_indices = random.sample(range(len(dataset)), min(num_samples, len(dataset)))
+    sampled_items = [dataset[idx] for idx in sampled_indices]
+    data_with_fields = []
+    sampled_indices = list(range(len(sampled_indices)))
+    with open(output_file, 'w', encoding='utf-8') as outfile:
+        for idx,item in enumerate(sampled_items):
+        
+            # Create True Pair
+            input = item["func_documentation_string"] + " [SEP] " + item["func_code_string"]
+            target = 1
+            target_options = [0, 1]
+            
+            data_with_fields.append({
+                "input": input,
+                "target": target,
+                "target_options": target_options
+            })
+            
+            # Create False Pair
+            sampled_indices.remove(idx)  # To avoid sampling from the same line
+            sampled_idx = random.choice(sampled_indices) # Randomly sample code from another line
+            input = item["func_documentation_string"] + " [SEP] " + sampled_items[sampled_idx]["func_code_string"]
+            sampled_indices.append(idx)  # Add back the removed index for future sampling
+
+            target = 0
+            target_options = [0, 1]
+
+            data_with_fields.append({
+                "input": input,
+                "target": target,
+                "target_options": target_options
+        })
+        for item in data_with_fields:
+                json.dump(item, outfile, ensure_ascii=False)
+                outfile.write('\n')
+
+def create_cbt_jsonl_file(input_file, output_file):
+    data_with_fields = []
+    data = []
+
+    # Load data from the input JSONL file
+    with open(input_file, 'r', encoding='utf-8') as infile:
+        kaki = infile
+        items = ast.literal_eval(kaki[1])
+        for item_dict in items:
+            #print(item_dict)
+            data.append(item_dict)
+
+    # Create a list of indices to sample from
+    sample_indices = list(range(len(data)))
+    for idx, item in enumerate(data):
+        
+        # Create True Pair
+        input = item["func_documentation_string"] + " [SEP] " + item["func_code_string"]
+        target = 1
+        target_options = [0, 1]
+        
+        data_with_fields.append({
+            "input": input,
+            "target": target,
+            "target_options": target_options
+        })
+        
+        # Create False Pair
+        sample_indices.remove(idx)  # To avoid sampling from the same line
+        sampled_idx = random.choice(sample_indices) # Randomly sample code from another line
+        input = item["docstring"] + " [SEP] " + data[sampled_idx]["code"]
+        sample_indices.append(idx)  # Add back the removed index for future sampling
+
+        target = 0
+        target_options = [0, 1]
+
+        data_with_fields.append({
+            "input": input,
+            "target": target,
+            "target_options": target_options
+        })
+
+    with open(output_file, 'w', encoding='utf-8') as outfile:
+        for item in data_with_fields:
+            json.dump(item, outfile, ensure_ascii=False)
+            outfile.write('\n')
+
+def main():
+
+    
+    num_lines_to_select = 10
+    
+    input_file = load_dataset("code_search_net", "ruby")["test"]
+    output_file = 'test_sample_cbt.json'
+    cbt_format_file = "test_sample_cbt.jsonl"
+            
+    sample_create_cbt_jsonl_file(input_file, output_file,10)
+    
+if __name__ == '__main__':
+    main()
